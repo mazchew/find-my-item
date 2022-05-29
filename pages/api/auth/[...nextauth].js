a068ca6416b8720ccc7e68d037f0ec9a7782b5e7
Howardta1 <zhouyx2000@gmail.com>
2022-05-29 22:49:24 +0800

    added ignoreTLS

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,22 +1,22 @@
-import nodemailer from 'nodemailer'
-import NextAuth from 'next-auth'
-import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
-import EmailProvider from 'next-auth/providers/email'
-import MongoClientPromise from './lib/mongodb'
+import nodemailer from "nodemailer";
+import NextAuth from "next-auth";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import EmailProvider from "next-auth/providers/email";
+import MongoClientPromise from "./lib/mongodb";
 
-const THIRTY_DAYS = 30 * 24 * 60 * 60
-const THIRTY_MINUTES = 30 * 60
+const THIRTY_DAYS = 30 * 24 * 60 * 60;
+const THIRTY_MINUTES = 30 * 60;
 
 export default NextAuth({
   pages: {
-    signIn: '/login',
-    verifyRequest: '/verify-request'
+    signIn: "/login",
+    verifyRequest: "/verify-request",
   },
   secret: process.env.SECRET,
   session: {
-    strategy: 'jwt',
+    strategy: "jwt",
     maxAge: THIRTY_DAYS,
-    updateAge: THIRTY_MINUTES
+    updateAge: THIRTY_MINUTES,
   },
   adapter: MongoDBAdapter(MongoClientPromise),
   providers: [
@@ -26,32 +26,33 @@ export default NextAuth({
         port: process.env.EMAIL_SERVER_PORT,
         auth: {
           user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD
-        }
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+        ignoreTLS: true,
       },
       from: process.env.EMAIL_FROM,
-      async sendVerificationRequest ({
+      async sendVerificationRequest({
         identifier: email,
         url,
-        provider: { server, from }
+        provider: { server, from },
       }) {
-        const { host } = new URL(url)
-        const transport = nodemailer.createTransport(server)
+        const { host } = new URL(url);
+        const transport = nodemailer.createTransport(server);
         await transport.sendMail({
           to: email,
           from,
           subject: `Sign in to ${host}`,
           text: text({ url, host }),
-          html: html({ url, host, email })
-        })
-      }
-    })
-  ]
-})
+          html: html({ url, host, email }),
+        });
+      },
+    }),
+  ],
+});
 
-function html ({ url, host, email }) {
-  const escapedEmail = `${email.replace(/\./g, '&#8203;.')}`
-  const escapedHost = `${host.replace(/\./g, '&#8203;.')}`
+function html({ url, host, email }) {
+  const escapedEmail = `${email.replace(/\./g, "&#8203;.")}`;
+  const escapedHost = `${host.replace(/\./g, "&#8203;.")}`;
   // Your email template here
   return `
       <body>
@@ -60,10 +61,10 @@ function html ({ url, host, email }) {
         <p>
           <a href="${url}">Sign in to ${escapedHost}</a>
       </body>
-  `
+  `;
 }
 
 // Fallback for non-HTML email clients
-function text ({ url, host }) {
-  return `Sign in to ${host}\n${url}\n\n`
+function text({ url, host }) {
+  return `Sign in to ${host}\n${url}\n\n`;
 }
