efba28313368214449369063210c6f4643d680eb
Howardta1 <zhouyx2000@gmail.com>
2022-06-14 18:48:46 +0800

    Items card and grid created

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,8 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-xl font-medium text-gray-800">
-        Item Gallery
-      </h1>
-      <p className="text-gray-500">
-        Check out all found items
-      </p>
+      <h1 className="text-xl font-medium text-gray-800">Item Gallery</h1>
+      <p className="text-gray-500">Check out all found items</p>
     </div>
   );
 }
