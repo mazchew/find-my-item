bcca105efb0095788740c93d88f576f7b3ac6f2a
mazchew <masonchew@u.nus.edu>
2022-06-20 22:46:14 +0800

    started styling for imageupload component

--- components/ImageUpload.js
+++ components/ImageUpload.js
@@ -3,15 +3,35 @@ import Image from 'next/image';
 
 
 const ImageUpload = ({
-    label = 'Image',
-    initialImg = null,
-    objectFit = 'cover',
-    accept = '.png, .jpg, .jpeg,',
+    label = "Image",
+    initialImage = null,
+    objectFit = "cover",
+    accept = ".png, .jpg, .jpeg,",
     sizeLimit = 5000000,
 }) => {
 
+    const [image, setImage] = useState(null);
+
 
     return (
+        <div>
+            <label className="text-gray-600">Image</label>
+            <button className="relative aspect-w-16 aspect-h-9 overflow-hidden rounded-md transition group focus:outline-none hover:opacity-50">
+                {image?.src ? (
+                    <Image
+                    src={image.src}
+                    alt={image?.alt ?? ""}
+                    layout="fill"
+                    objectFit="cover"
+                    />
+                ) : null}
+
+                <div>
+                    
+                </div>   
+
+            </button>
+        </div>
     );
 }
 
