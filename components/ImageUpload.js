08f3f232c1ea3bac39f2b62efd6d534667009435
mazchew <masonchew@u.nus.edu>
2022-06-20 21:19:28 +0800

    building out imageupload component

--- components/ImageUpload.js
+++ components/ImageUpload.js
@@ -2,4 +2,18 @@ import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 
 
-const ImageUpload = {}
+const ImageUpload = ({
+    label = 'Image',
+    initialImg = null,
+    objectFit = 'cover',
+    accept = '.png, .jpg, .jpeg,',
+    sizeLimit = 5000000,
+}) => {
+
+
+    return (
+    );
+}
+
+
+export default ImageUpload;
