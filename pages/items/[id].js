4ebe62887d14b4929bab02edb3546b47bb306298
mazchew <masonchew@u.nus.edu>
2022-06-21 23:10:45 +0800

    started individual item page

--- pages/items/[id].js
+++ pages/items/[id].js
@@ -1,3 +1,14 @@
 import Image from 'next/image';
 import Layout from '@/components/Layout';
 
+const PostedItem = () => {
+    return (
+        <Layout>
+            <h3>item page</h3>
+        </Layout>
+    )
+
+
+};
+
+export default PostedItem;
