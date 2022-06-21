e8f4e8408814b8820b938f722ce1fd2ff0ed2df8
mazchew <masonchew@u.nus.edu>
2022-06-21 17:37:21 +0800

    imported supabase and started on handler function

--- pages/api/upload-image.js
+++ pages/api/upload-image.js
@@ -1,4 +1,16 @@
+import { createClient } from '@supabase/supabase-js';
 
-export default function handler() {
-    
+const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY
+);
+
+
+export default async function handler(req, res) {
+    if (req.method === "POST") {
+
+    } else {
+        res.setHeader("Allow", ["POST"]);
+        res.status(405).json({ message: `method not supported` });
+    }
 }
