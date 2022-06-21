9521438bc0257d731d9bdf9a99b09dcc9bbe5300
Howardta1 <zhouyx2000@gmail.com>
2022-06-21 17:46:14 +0800

    Added PropTypes validation for Input

--- components/Input.js
+++ components/Input.js
@@ -1,8 +1,18 @@
 import PropTypes from "prop-types";
+import classNames from "classnames";
 import { useField } from "formik";
 
-const Input = () => {
+const Input = ({ type = "", label = "", className = "", ...props }) => {
+  const [field, meta] = useField(props);
+  const error = meta?.touched && meta?.error;
+
   return <div>Input</div>;
+};
+
+Input.propTypes = {
+  type: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  className: PropTypes.string,
 };
 
 export default Input;
