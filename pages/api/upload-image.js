6d91f3934c5e968090ab23d78e20629f4dd4acbd
mazchew <masonchew@u.nus.edu>
2022-06-21 18:12:25 +0800

    error handling

--- pages/api/upload-image.js
+++ pages/api/upload-image.js
@@ -1,4 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
+import { nanoid } from 'nanoid';
+import { decode } from 'base64-arraybuffer';
+import { data } from 'autoprefixer';
 
 const supabase = createClient(
     process.env.SUPABASE_URL,
@@ -13,13 +16,38 @@ export default async function handler(req, res) {
         if (!image) {
             return res.status(500).json({ message: "No image provided" });
         }
+        try {
+            const contentType = image.match(/data:(.*);base64/)?.[1];
+            const base64FileData = image.split('base64,')?.[1];
+    
+            if (!contentType || !base64FileData) {
+                return res.status(500).json({ message: "Invalid image" });
+            }
 
-        const contentType = image.match(/data:(.*);base64/)?.[1];
-        const base64FileData = image.split('base64,')?.[1];
+            const fileName = nanoid();
+            const ext = contentType.split('/')[1];
+            const path = `${fileName}.${ext}`;
+      
+            const { data, error: uploadError } = await supabase.storage
+              .from(process.env.SUPABASE_BUCKET)
+              .upload(path, decode(base64FileData), {
+                contentType,
+                upsert: true,
+            });
 
-        if (!contentType || !base64FileData) {
-            return res.status(500).json({ message: "Invalid image" });
-          }
+            if (uploadError) {
+                console.log(uploadError);
+                throw new Error('Unable to upload image to storage');
+            }
+
+            const url = `${process.env.SUPABASE_URL}/storage/v1/object/public/${data.Key}`;
+            return res.status(200).json({url});
+
+        } catch (error) {
+            res.status(500).json({ message: "Error" })
+        }
+
+
 
     } else {
         res.setHeader("Allow", ["POST"]);
