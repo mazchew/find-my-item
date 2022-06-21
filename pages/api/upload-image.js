20aa263cc85d77ce0f65eee74b7581b5c55f5e12
mazchew <masonchew@u.nus.edu>
2022-06-21 17:50:35 +0800

    handling invalid images

--- pages/api/upload-image.js
+++ pages/api/upload-image.js
@@ -8,9 +8,21 @@ const supabase = createClient(
 
 export default async function handler(req, res) {
     if (req.method === "POST") {
+        let {image} = req.body;
+
+        if (!image) {
+            return res.status(500).json({ message: "No image provided" });
+        }
+
+        const contentType = image.match(/data:(.*);base64/)?.[1];
+        const base64FileData = image.split('base64,')?.[1];
+
+        if (!contentType || !base64FileData) {
+            return res.status(500).json({ message: "Invalid image" });
+          }
 
     } else {
         res.setHeader("Allow", ["POST"]);
-        res.status(405).json({ message: `method not supported` });
+        res.status(405).json({ message: "Method not supported" });
     }
 }
