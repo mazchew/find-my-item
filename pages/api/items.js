aa16a89f05de434feb456b51811749c462bbe087
mazchew <masonchew@u.nus.edu>
2022-06-23 01:11:12 +0800

    bug fixes; removed unnecessary migration

--- pages/api/items.js
+++ pages/api/items.js
@@ -22,7 +22,6 @@ export default async function handler(req, res) {
           description,
           location,
           category,
-          ownerId: user.id,
         },
       });
       res.status(200).json(item);
--- prisma/migrations/20220622160904_test/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Changed the type of `category` on the `Item` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
-
-*/
--- AlterTable
-ALTER TABLE "Item" DROP COLUMN "category",
-ADD COLUMN     "category" TEXT NOT NULL;
