38cda7c65e9e61eb0e9ac209a5477335e7e1661c
mazchew <masonchew@u.nus.edu>
2022-06-22 17:50:50 +0800

    connected prisma to [id].js file

--- pages/items/[id].js
+++ pages/items/[id].js
@@ -1,6 +1,9 @@
 import Image from 'next/image';
 import Layout from '@/components/Layout';
 import { LocationMarkerIcon } from "@heroicons/react/solid";
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 const PostedItem = (item = null) => {
     return (
@@ -8,27 +11,57 @@ const PostedItem = (item = null) => {
             <div className="max-w-screen-lg mx-auto">
                 <div className="flex flex-col sm:flex-row sm:justify-between sm:space-x-4 space-y-4">
                     <div>
-                        <h1 className="text-2xl font-semibold truncate">Item Title</h1>
+                        <h1 className="text-2xl font-semibold truncate">{item?.title ?? ''}</h1>
                         <ol className="inline-flex items-center space-x-1 text-gray-500">
                             <li className="inline-flex items-center space-x-0.5">
                                 <LocationMarkerIcon className="h-4 w-4" />
                                 <span>
-                                        test location  · 
+                                    {item?.location ?? ''}  · 
                                 </span>
                             </li>
                             <li>
-                                <span>test category</span>
+                                <span>{item?.category ?? ''}</span>
                             </li>
                         </ol>
                     </div>
                 </div>
 
-                <p className="mt-8 text-lg">item description</p>
+                <p className="mt-8 text-lg">{item?.description ?? ''}</p>
             </div>
         </Layout>
     )
-
-
 };
+
+export async function getStaticPaths() {
+    const items = await prisma.item.findMany({
+        select: { id: true },
+      });
+    
+      return {
+        paths: items.map(item => ({
+          params: { id: item.id },
+        })),
+        fallback: false,
+      };
+}
+
+export async function getStaticProps({ params }) {
+    const item = await prisma.item.findUnique({
+      where: { id: params.id },
+    });
+  
+    if (item) {
+      return {
+        props: JSON.parse(JSON.stringify(item)),
+      };
+    }
+  
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
 
 export default PostedItem;
