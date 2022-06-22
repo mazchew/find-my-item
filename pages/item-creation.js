7842ff819271caf653f9f0c758f6c141fbed07b7
mazchew <masonchew@u.nus.edu>
2022-06-22 15:15:42 +0800

    added item creation page

--- pages/item-creation.js
+++ pages/item-creation.js
@@ -1,1 +1,27 @@
 import Layout from '@/components/Layout';
+import ItemForm from '@/components/ItemForm'
+import axios from 'axios';
+
+const CreateItem = () => {
+    const createItem = data => axios.post('api/items', data)
+
+    return (
+        <Layout>
+            <div className="max-w-screen-sm mx-auto">
+                <h1 className="text-xl font-medium text-gray-800">Post new item</h1>
+                <p className="text-gray-500">
+                    Fill in the form below to post a new item.
+                </p>
+                <div className="mt-8">
+                <ItemForm
+                    buttonText="Add item"
+                    redirectPath="/"
+                    onSubmit={createItem}
+                />
+                </div>
+            </div>
+        </Layout>
+    );
+};
+
+export default CreateItem;
