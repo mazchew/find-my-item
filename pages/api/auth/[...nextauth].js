f656c4f90b834886079f067d463b33454a7dc674
mazchew <masonchew@u.nus.edu>
2022-06-23 18:20:27 +0800

    attempted bug fixing for auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -43,6 +43,10 @@ export default NextAuth({
         signIn: '/signin',
         verifyRequest: '/verifyemail',
     },
+    secret: process.env.NEXTAUTH_SECRET,
+    session: {
+      strategy:"jwt",
+    },
     providers: [
       EmailProvider({
         server: {
@@ -58,6 +62,6 @@ export default NextAuth({
         sendVerificationRequest,
       }),
     ],
-    secret: process.env.NEXTAUTH_SECRET,
+    useSecureCookies:false,
     adapter: PrismaAdapter(prisma),
 });
