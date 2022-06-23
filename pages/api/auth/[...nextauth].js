993e6f0b5a1ccef45f292ab0a5736c07e89f8847
mazchew <masonchew@u.nus.edu>
2022-06-23 16:42:04 +0800

    added debug option for nextauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -58,6 +58,7 @@ export default NextAuth({
         sendVerificationRequest,
       }),
     ],
+    debug:true,
     secret: process.env.NEXTAUTH_SECRET,
     adapter: PrismaAdapter(prisma),
   });
