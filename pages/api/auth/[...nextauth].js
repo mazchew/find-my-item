6db70b3b7560c5da83bde3ef28aee52c713fa93d
mazchew <masonchew@u.nus.edu>
2022-06-23 18:18:04 +0800

    attempted bug fixing

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,35 +1,35 @@
-import NextAuth from "next-auth";
-import EmailProvider from "next-auth/providers/email";
-import nodemailer from "nodemailer";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
-import Handlebars from "handlebars";
-import { readFileSync } from "fs";
-import path from "path";
+import NextAuth from 'next-auth';
+import EmailProvider from 'next-auth/providers/email';
+import nodemailer from 'nodemailer';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+import Handlebars from 'handlebars';
+import { readFileSync } from 'fs';
+import path from 'path';
 
 const prisma = new PrismaClient();
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_SERVER_HOST,
-  port: process.env.EMAIL_SERVER_PORT,
-  auth: {
-    user: process.env.EMAIL_SERVER_USER,
-    pass: process.env.EMAIL_SERVER_PASSWORD,
-  },
-  secure: true,
+    host: process.env.EMAIL_SERVER_HOST,
+    port: process.env.EMAIL_SERVER_PORT,
+    auth: {
+      user: process.env.EMAIL_SERVER_USER,
+      pass: process.env.EMAIL_SERVER_PASSWORD,
+    },
+    secure: true,
 });
 
-const emailsDir = path.resolve(process.cwd(), "emails");
+const emailsDir = path.resolve(process.cwd(), 'emails');
 
 const sendVerificationRequest = ({ identifier, url }) => {
-  const emailFile = readFileSync(path.join(emailsDir, "confirmation.html"), {
-    encoding: "utf8",
+  const emailFile = readFileSync(path.join(emailsDir, 'confirmation.html'), {
+    encoding: 'utf8',
   });
   const emailTemplate = Handlebars.compile(emailFile);
   transporter.sendMail({
     from: `"🔍 FindMyItem" ${process.env.EMAIL_FROM}`,
     to: identifier,
-    subject: "Your sign-in link for FindMyItem",
+    subject: 'Your sign-in link for FindMyItem',
     html: emailTemplate({
       base_url: process.env.NEXTAUTH_URL,
       signin_url: url,
@@ -39,14 +39,9 @@ const sendVerificationRequest = ({ identifier, url }) => {
 };
 
 export default NextAuth({
-<<<<<<< HEAD
     pages: {
         signIn: '/signin',
         verifyRequest: '/verifyemail',
-    },
-    secret: process.env.NEXTAUTH_SECRET,
-    session: {
-      strategy: "jwt",
     },
     providers: [
       EmailProvider({
@@ -63,31 +58,6 @@ export default NextAuth({
         sendVerificationRequest,
       }),
     ],
-    useSecureCookies: false,
+    secret: process.env.NEXTAUTH_SECRET,
     adapter: PrismaAdapter(prisma),
-  });
-=======
-  pages: {
-    signIn: "/signin",
-    verifyRequest: "/verifyemail",
-  },
-  providers: [
-    EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
-        },
-      },
-      from: process.env.EMAIL_FROM,
-      maxAge: 10 * 60,
-      sendVerificationRequest,
-    }),
-  ],
-  debug: true,
-  secret: process.env.NEXTAUTH_SECRET,
-  adapter: PrismaAdapter(prisma),
 });
->>>>>>> f01c3ded599c63b47d91e120355603ed849234b8
