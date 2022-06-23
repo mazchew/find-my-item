fe48cb66092fa8e2c974dd81ec46ffd5c4abedd3
mazchew <masonchew@u.nus.edu>
2022-06-23 23:01:24 +0800

    debug attempt

--- pages/api/upload-image.js
+++ pages/api/upload-image.js
@@ -49,7 +49,7 @@ export default async function handler(req, res) {
       const url = `${process.env.SUPABASE_URL}/storage/v1/object/public/${data.Key}`;
       return res.status(200).json({ url });
     } catch (error) {
-      res.status(500).json({ message: "Error" });
+      res.status(500).json({ message: error.data });
     }
   } else {
     res.setHeader("Allow", ["POST"]);
