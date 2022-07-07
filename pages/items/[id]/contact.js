694c1087423ce961a693970e58138c9aae826e6f
mazchew <masonchew@u.nus.edu>
2022-07-08 02:59:19 +0800

    handle contact process

--- pages/items/[id]/contact.js
+++ pages/items/[id]/contact.js
@@ -5,13 +5,13 @@ import axios from "axios";
 // import { prisma } from '@/lib/prisma';
 
 const Contact = (item = null) => {
-//   const handleOnSubmit = (data) => axios.post(`/api/items/${item.id}/contact`, data);
+  const handleOnSubmit = (data) => axios.post(`/api/items/${item.id}/contact`, data);
   return (
     <Layout>
       <div className="max-w-screen-sm mx-auto">
         <h1 className="text-xl font-medium text-gray-800">Contact Form</h1>
         <p className="text-grey-500">
-        Fill in the form below to contact the poster.
+            Fill in the form below to contact the poster.
         </p>
         <div className="mt-8">
         {item ? (
