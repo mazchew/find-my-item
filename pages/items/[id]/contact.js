962b2a06c805f3ad1c06e9f55727435c43920733
Howardta1 <zhouyx2000@gmail.com>
2022-07-08 16:05:18 +0800

    added contact button

--- pages/items/[id]/contact.js
+++ pages/items/[id]/contact.js
@@ -5,22 +5,24 @@ import axios from "axios";
 // import { prisma } from '@/lib/prisma';
 
 const Contact = (item = null) => {
-  const handleOnSubmit = (data) => axios.post(`/api/items/${item.id}/contact`, data);
+  const handleOnSubmit = (data) =>
+    axios.post(`/api/items/${item.id}/contact`, data);
   return (
     <Layout>
       <div className="max-w-screen-sm mx-auto">
         <h1 className="text-xl font-medium text-gray-800">Contact Form</h1>
         <p className="text-grey-500">
-            Fill in the form below to contact the poster.
+          Fill in the form below to contact the poster. The poster will receive
+          your Email and the Message below.
         </p>
         <div className="mt-8">
-        {item ? (
-            <ContactForm 
+          {item ? (
+            <ContactForm
               buttonText="Contact Poster"
               redirectPath={`/items/${item.id}`}
               onSubmit={handleOnSubmit}
             />
-        ) : null}
+          ) : null}
         </div>
       </div>
     </Layout>
