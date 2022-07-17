b60dfe4178e03087d37077c1073a126066395b38
mazchew <masonchew@u.nus.edu>
2022-07-18 00:16:54 +0800

    attempt to improve signin email times

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,46 +8,108 @@ import { readFileSync } from 'fs';
 import path from 'path';
 
 
-const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_SERVER_HOST,
-    port: process.env.EMAIL_SERVER_PORT,
-    auth: {
-      user: process.env.EMAIL_SERVER_USER,
-      pass: process.env.EMAIL_SERVER_PASSWORD,
-    },
-    secure: false,
-});
+// const transporter = nodemailer.createTransport({
+//     host: process.env.EMAIL_SERVER_HOST,
+//     port: process.env.EMAIL_SERVER_PORT,
+//     auth: {
+//       user: process.env.EMAIL_SERVER_USER,
+//       pass: process.env.EMAIL_SERVER_PASSWORD,
+//     },
+//     secure: false,
+// });
 
-const emailsDir = path.resolve(process.cwd(), 'emails');
+// const emailsDir = path.resolve(process.cwd(), 'emails');
 
-const sendVerificationRequest = ({ identifier, url }) => {
-  const emailFile = readFileSync(path.join(emailsDir, 'confirmation.html'), {
-    encoding: 'utf8',
-  });
-  const emailTemplate = Handlebars.compile(emailFile);
-  transporter.sendMail({
-    from: `"🔍 FindMyItem" <${process.env.EMAIL_FROM}>`,
-    to: identifier,
-    subject: 'Your sign-in link for FindMyItem',
-    html: emailTemplate({
-      base_url: process.env.NEXTAUTH_URL,
-      signin_url: url,
-      email: identifier,
-    }),
-  });
-};
+// const sendVerificationRequest = ({ identifier, url }) => {
+//   const emailFile = readFileSync(path.join(emailsDir, 'confirmation.html'), {
+//     encoding: 'utf8',
+//   });
+//   const emailTemplate = Handlebars.compile(emailFile);
+//   transporter.sendMail({
+//     from: `"🔍 FindMyItem" <${process.env.EMAIL_FROM}>`,
+//     to: identifier,
+//     subject: '✔️ Your sign-in link for FindMyItem',
+//     html: emailTemplate({
+//       base_url: process.env.NEXTAUTH_URL,
+//       signin_url: url,
+//       email: identifier,
+//     }),
+//   });
+// };
+
+// export default NextAuth({
+//     pages: {
+//         signIn: '/signin',
+//         verifyRequest: '/verifyemail',
+//     },
+//     secret: process.env.NEXTAUTH_SECRET,
+//     providers: [
+//       EmailProvider({
+//         maxAge: 10 * 60,
+//         sendVerificationRequest,
+//       }),
+//     ],
+//     adapter: PrismaAdapter(prisma),
+// });
 
 export default NextAuth({
-    pages: {
-        signIn: '/signin',
-        verifyRequest: '/verifyemail',
-    },
-    secret: process.env.NEXTAUTH_SECRET,
-    providers: [
-      EmailProvider({
-        maxAge: 10 * 60,
-        sendVerificationRequest,
-      }),
-    ],
-    adapter: PrismaAdapter(prisma),
-});
+  pages: {
+    signIn: '/signin',
+    verifyRequest: '/verifyemail',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    maxAge: 10 * 60,
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+        secure: false,
+      },
+      async sendVerificationRequest({
+        identifier,
+        url,
+        provider: { server },
+      }) {
+        const emailsDir = path.resolve(process.cwd(), 'emails');
+        const emailFile = readFileSync(path.join(emailsDir, 'confirmation.html'), {
+          encoding: 'utf8',
+        });
+        const emailTemplate = Handlebars.compile(emailFile);
+        const transporter = nodemailer.createTransport(server);
+        await transporter.sendMail({
+          from: `"🔍 FindMyItem" <${process.env.EMAIL_FROM}>`,
+          to: identifier,
+          subject: "✅ Your sign-in link for FindMyItem",
+          html: emailTemplate({
+            base_url: process.env.NEXTAUTH_URL,
+            signin_url: url,
+            email: identifier,
+          }),
+        });
+      },
+    }),
+  ],
+  // providers: [
+  //   server: {
+  //     host: process.env.EMAIL_SERVER_HOST,
+  //     port: process.env.EMAIL_SERVER_PORT,
+  //     auth: {
+  //       user: process.env.EMAIL_SERVER_USER,
+  //       pass: process.env.EMAIL_SERVER_PASSWORD,
+  //     },
+  //   },
+  //   async 
+  // ],
+
+
+
+
+})
