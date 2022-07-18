c336f5154bd468a4c44f5b742773c85ba920df87
mazchew <masonchew@u.nus.edu>
2022-07-19 00:19:47 +0800

    resolved merge conflicts and edited layout test, layout and card component

--- __tests__/layout.test.js
+++ __tests__/layout.test.js
@@ -2,22 +2,20 @@ import { render, screen } from "@testing-library/react";
 import Layout from "../components/Layout";
 import "@testing-library/jest-dom";
 import React from "react";
-import { useSession, signOut } from "next-auth/react";
 import "@testing-library/jest-dom";
-// jest.mock("next-auth/react");
 
 
 jest.mock("next-auth/react", () => {
-    const originalModule = jest.requireActual('next-auth/react');
+    const originalModule = jest.requireActual("next-auth/react");
     const mockSession = {
       expires: new Date(Date.now() + 2 * 86400).toISOString(),
-      user: { email: "a" }
+      user: { email: "a" },
     };
     return {
       __esModule: true,
       ...originalModule,
       useSession: jest.fn(() => {
-        return {data: mockSession, status: 'authenticated'}  // return type is [] in v3 but changed to {} in v4
+        return {data: mockSession, status: "authenticated"}  // return type is [] in v3 but changed to {} in v4
       }),
     };
   });
@@ -28,8 +26,7 @@ describe("Layout", () => {
     render(<Layout />);
 
     expect(screen.getByText("FindMyItem")).toBeInTheDocument();
-    // expect(screen.getByText("Gallery")).toBeInTheDocument();
-    // expect(screen.getByText("Noticeboard")).toBeInTheDocument();
-    // expect(screen.getByText("Sign In")).toBeInTheDocument();
+    expect(screen.getByText("Gallery")).toBeInTheDocument();
+    expect(screen.getByText("NoticeBoard")).toBeInTheDocument();
   });
 });
