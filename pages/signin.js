9beccfc665d3de5fcd9821c95e9fc3b228a84dbd
mazchew <masonchew@u.nus.edu>
2022-07-23 19:25:14 +0800

    added email signin regex

--- pages/signin.js
+++ pages/signin.js
@@ -12,22 +12,6 @@ const SignInSchema = Yup.object().shape({
     .required("This field is required"),
 });
 
-const ItemSchema = Yup.object().shape({
-  title: Yup.string()
-    .trim()
-    .matches(/^[\w\-\/\n,.!?'"$#@():; ]*$/, "Invalid characters")
-    .required(),
-  location: Yup.string()
-    .trim()
-    .matches(/^[\w\-\/\n,.!?'"$#@():; ]*$/, "Invalid characters")
-    .required(),
-  description: Yup.string()
-    .trim()
-    .matches(/^[\w\-\/\n,.!?'"$#@():; ]*$/, "Invalid characters")
-    .required(),
-  category: Yup.string().required(),
-});
-
 const Signin = () => {
   const [email, setEmail] = useState("");
 
